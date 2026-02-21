57ca8519c7f692dc0ec5d9afc4376ebac7d0c6db
takazato3 <hazystar0601@gmail.com>
2026-02-21 16:25:50 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-     GOOGLE_MAPS_API_KEY: 'AIzaSyDjDm-XDTKzb-4XiG8Jtgnwy-GoicVTovg',  // ← ここにあなたのAPI Key全体を貼り付け
+     GOOGLE_MAPS_API_KEY: 'AIzaSyDjDm-XDTKzb-4XiG8Jtgnwy-GoicVTovg'  // ← ここにあなたのAPI Key全体を貼り付け
    };
 ```
 
