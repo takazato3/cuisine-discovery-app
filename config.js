2c7a009175ccafca2a227ccc983795c932640877
takazato3 <hazystar0601@gmail.com>
2026-02-21 16:13:11 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+     GOOGLE_MAPS_API_KEY: 'AIzaSyDjDm-XDTKzb-4XiG8Jtgnwy-GoicVTovg',
+   };
+```
+5. **「Commit changes」**
+
+3分待って、以下にアクセス:
+```
+https://takazato3.github.io/cuisine-discovery-app/
